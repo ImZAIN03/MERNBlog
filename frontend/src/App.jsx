79ae2cf77db430392475c34f948204ba6c6074f3
Muhammed Zain <muhammedzain2256@gmail.com>
2023-11-17 20:37:48 +0500

    Profile and Post Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Register from './pages/Register';
 import PostDetails from './pages/PostDetails';
 import CreatePost from './pages/CreatePost';
 import EditPost from './pages/EditPost';
+import Profile from './pages/Profile';
 
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
           <Route exact path="/write" element={<CreatePost/>}/>
           <Route exact path="/posts/post/:id" element={<PostDetails/>}/>
           <Route exact path="/edit/:id" element={<EditPost/>}/>
+          <Route exact path="/profile/:id" element={<Profile/>}/>
         </Routes>
     </div>
   )
