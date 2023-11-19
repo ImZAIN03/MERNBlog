6200204b79e1f42f09eb30b7a9128106b5d0cea0
Muhammed Zain <muhammedzain2256@gmail.com>
2023-11-19 22:59:45 +0500

    Routes

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
 const postRoute = require('./routes/posts')
+const commentRoute = require('./routes/comment')
 
 //database
 const connectDB=async()=>{
@@ -24,6 +25,7 @@ app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/posts", postRoute)
+app.use("/api/comments", commentRoute)
 
 app.listen(process.env.PORT, ()=>{
     connectDB()
