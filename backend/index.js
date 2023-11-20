b2d82d074b043715d27d1738600488cf997658a3
Muhammed Zain <muhammedzain2256@gmail.com>
2023-11-20 11:30:12 +0500

    JsonWebToken (JWT)

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const authRoute = require('./routes/auth')
-const userRoute = require('./routes/users')
-const postRoute = require('./routes/posts')
-const commentRoute = require('./routes/comment')
+const cookieParser = require('cookie-parser');
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
+const commentRoute = require('./routes/comment');
 
 //database
 const connectDB=async()=>{
@@ -22,6 +23,7 @@ const connectDB=async()=>{
 //middlewares
 dotenv.config();
 app.use(express.json())
+app.use(cookieParser())
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/posts", postRoute)
