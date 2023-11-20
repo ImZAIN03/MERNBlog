ae5d6796073f20482e76148b7a869bb69d8d6bf0
Muhammed Zain <muhammedzain2256@gmail.com>
2023-11-20 22:18:29 +0500

    Login And Register Functionality

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors')
 const cookieParser = require('cookie-parser');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
@@ -24,6 +25,7 @@ const connectDB=async()=>{
 dotenv.config();
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({origin:"http://localhost:5173", credentials: true}))
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/posts", postRoute)
